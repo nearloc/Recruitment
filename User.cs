4f2d5ae2e60194e1a18476bc0b859d66cbbc3e3d
Sinan Taviloglu <staviloglu@gmail.com>
2020-12-17 17:05:08 +0300

    initial commit

---
+++ User.cs
@@ -0,0 +1,10 @@
+﻿namespace nearloc.isealim.mqtt.broker.console
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
+
